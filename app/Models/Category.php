69ebc1bce52ea0c1aa303e11bd9051515ce5d755
Ouali Mohamed <mohamed.ouali2010gmail.com>
2025-04-04 15:03:29 +0100

    Creating factories

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -2,7 +2,7 @@
 
 namespace App\Models;
 
-use Illulnate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Category extends Model
