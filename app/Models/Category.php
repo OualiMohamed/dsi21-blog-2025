e81d6cfd1294db520f90737b871dd6cb7a91f514
Ouali Mohamed <mohamed.ouali2010gmail.com>
2025-04-04 14:04:06 +0100

    create models, migrations & writing schema

---
+++ app/Models/Category.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illulnate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Category extends Model
+{
+    use HasFactory;
+}
